d71909dff0735e5ffaad1af3e4be043feda7cb07
Syafiq Hanafee <me@iamfeek.com>
2016-09-28 17:46:27 +0800

    Fixed source URL

--- imports/api/email.js
+++ imports/api/email.js
@@ -24,6 +24,7 @@ Accounts.emailTemplates.verifyEmail = {
   text( user, url ) {
     let emailAddress   = user.emails[0].address,
         urlWithoutHash = url.replace( '#/', '' ),
+        urlProper = url.replace("pynook.herokuapp.com", "pynook.com")
         supportEmail   = "support@pynook.com",
         emailBody      = `To verify your email address (${emailAddress}) visit the following link:\n\n${url}\n\n If you did not request this verification, please ignore this email. If you feel something is wrong, please contact our support team: ${supportEmail}.`;
 

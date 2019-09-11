7968128766f356dec0c5f6648bc0d19688b34125
Muhammad Syafiq Hanafee <iamfeek@users.noreply.github.com>
2019-09-11 10:18:32 +0800

    First attempt at the workflow with the given branches. (#3)
    * Scaffolding of build operations
    
    * adding of .netlify and .idea

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+    import 'bulma/css/bulma.css'
+    import { Button } from 'svelma';
+	export let name;
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<div>
+    <h1>Hello {name}!</h1>
+    <Button type="is-primary">I'm a Button!</Button>
+</div>
